f7f1e1478f2a5413aaf605e31ddeaa4b8c1162ac
pcyrc <cannra@gmail.com>
2017-03-08 11:15:34 +0000

    adding javascript link

---
+++ script.js
@@ -0,0 +1,21 @@
+
+//put javescript in this file//
+
+	$(document).ready(function(){
+
+		 var button = $(".cookedbutton");
+
+
+		button.on('click', function(){
+
+			if (button.text() == "Tried this?")
+			{
+				button.html("Cooked!");
+				alert("Well Done!");
+			}
+			else
+			{
+				button.html("Tried this?");
+			}
+		})
+	})
