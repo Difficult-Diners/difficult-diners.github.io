a90789c20ee1b8c48e4ed265059b1566dc744feb
Aisha <aishasobey@Aishas-MacBook-Pro.local>
2017-03-14 15:06:47 +0000

    Starting putting dessert page together :)

---
+++ script-desserts.js
@@ -0,0 +1,16 @@
+//put desert script here//
+
+$(document).ready(function(){
+
+   var button = $(".cookedbutton");
+
+   button.on('click', function(){
+
+    if (button.text() == "Tried this?") {
+      button.html("Cooked!");
+      alert("Well Done!");
+    }
+    else {
+      button.html("Tried this?");
+    }
+  });
