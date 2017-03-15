49c0fb89ee86d067b735a4e01626689050ded3a3
alicesparks <alice.sparks@student.manchester.ac.uk>
2017-03-15 23:20:59 +0000

    Add

--- script-home.js
+++ script-home.js
@@ -36,3 +36,18 @@
 			});
 
 });
+
+window.onscroll = function () {scrollFunction()};
+
+function scrollFunction(){
+	if (document.body.scrollTop > 20 ||
+		document.documentElement.scrollTop > 20) {
+		document.getElementById("myBtn").style.display = "block";
+} else {
+	document.getElemntById("myBtn").style.display = "none";
+	}
+}
+ function topFunction(){
+ 	document.body.scrollTop = 0;
+ 	document.documentElement.scrollTop = 0;
+}
